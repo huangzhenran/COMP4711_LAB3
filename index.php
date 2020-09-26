fd96a44247e6172e0b60709debef8ff9b6b7153e
huangzhenran <1214084237@qq.com>
2020-09-26 16:19:50 +0800

    add a status property in student.php.And show four students infomation

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ $students = array();
 $first = new Student();
 $first->surname = "Doe";
 $first->first_name = "John";
+$first->status="Freshman";
 $first->add_email('home','john@doe.com');
 $first->add_email('work','jdoe@mcdonalds.com');
 $first->add_grade(65);
@@ -17,6 +18,7 @@ $students['j123'] = $first;
 $second = new Student();
 $second->surname = "Einstein";
 $second->first_name = "Albert";
+$second->status="Senior";
 $second->add_email('home','albert@braniacs.com');
 $second->add_email('work1','a_einstein@bcit.ca');
 $second->add_email('work2','albert@physics.mit.edu');
@@ -26,16 +28,25 @@ $second->add_grade(50);
 $students['a456'] = $second;
 
 
-$second = new Student();
-$second->surname = "zhenran";
-$second->first_name = "Huang";
+$third = new Student();
+$third->surname = "zhenran";
+$third->first_name = "Huang";
+$third->status="Freshman";
+$third->add_email('work1','1214084237@qq.com');
+$third->add_grade(95);
+$third->add_grade(80);
+$third->add_grade(50);
+$students['a426'] = $third;
 
-$second->add_email('work1','1214084237@qq.com');
-
-$second->add_grade(95);
-$second->add_grade(80);
-$second->add_grade(50);
-$students['a458'] = $second;
+$fourth = new Student();
+$fourth->surname = "hua";
+$fourth->first_name = "Lee";
+$fourth->status="Junior";
+$fourth->add_email('work1','1214344237@qq.com');
+$fourth->add_grade(95);
+$fourth->add_grade(80);
+$fourth->add_grade(50);
+$students['a468'] = $fourth;
 
 
 $stu=new Student();
